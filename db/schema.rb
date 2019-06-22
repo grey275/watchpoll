33afdf7ceb5c649fab74e9fa639fae9558ec7efc
nvngsys <jack.brennan@nvngsystems.com>
2019-06-22 09:59:57 -0700

    Creation of user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_161523) do
+ActiveRecord::Schema.define(version: 2019_06_22_164742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2019_06_22_161523) do
     t.string "name"
     t.string "seed_playlist_id"
     t.time "runtime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
