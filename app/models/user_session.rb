5d546d96297abe6adf3a19e0b4c28d7f8af0f2d8
nvngsys <jack.brennan@nvngsystems.com>
2019-06-22 10:08:16 -0700

    Create user session model

---
+++ app/models/user_session.rb
@@ -0,0 +1,4 @@
+class UserSession < ApplicationRecord
+  belongs_to :room
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_22_164742) do
+ActiveRecord::Schema.define(version: 2019_06_22_170321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,23 @@ ActiveRecord::Schema.define(version: 2019_06_22_164742) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_sessions", force: :cascade do |t|
+    t.bigint "room_id"
+    t.bigint "user_id"
+    t.datetime "start"
+    t.datetime "end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["room_id"], name: "index_user_sessions_on_room_id"
+    t.index ["user_id"], name: "index_user_sessions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "user_sessions", "rooms"
+  add_foreign_key "user_sessions", "users"
 end
