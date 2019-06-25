6317e538c23110dc7e5b1bdc14cb0a027523343e
grey275 <pjvanderpol@gmail.com>
2019-06-25 11:55:47 -0700

    standings now uses last preference order per active session

--- app/models/video_poll.rb
+++ app/models/video_poll.rb
@@ -1,39 +1,40 @@
 class VideoPoll < ApplicationRecord
   belongs_to :room
-  has_many :candidate_videos
   has_many :preference_orders
+  has_many :user_sessions
+  has_many :candidate_videos
 
   def standings
+    active_user_sessions = self.room.user_sessions.where(end: nil)
 
-    user_sessions = self.preference_orders.user_sessions
-
-    user_preference_orders = user_sessions.map do |user_session|
-      user_session.preference_orders.last
-    end
-
-    video_points_hash = self.candidate_videos.inject({}) do |video_points_hash, video|
-      video_points_hash[video.id] = 0
-      video_points_hash
-    end
-
-    user_preference_orders
-      .each do |order|
-        user_preferences = order.preferences
-        user_preferences
-          .each do |preference|
-            points = preference.position.nil? \
-              ? 0
-              : user_preferences.length - preference.position
-            puts 'position: ' + preference.position.to_s
-            puts 'points: ' + points.to_s
-            video_points_hash[preference.candidate_video_id] += points
-          end
+    active_session_preferences = active_user_sessions
+      .map do |user_session|
+        user_session.preference_orders.last.preferences
       end
+      .flatten
 
-    video_points_hash.keys.map do |key|
-      points = video_points_hash[key]
-      video_uid = CandidateVideo.find(key).video_uid
-      {video_id: key, video_uid: video_uid, points: points}
+    video_points_hash = tally self.candidate_videos, active_session_preferences
+
+    video_points_hash
+      .keys
+      .map do |key|
+        points = video_points_hash[key]
+        video_uid = CandidateVideo.find(key).video_uid
+        {video_id: key, video_uid: video_uid, points: points}
+      end
+      .sort_by {|video_standing| -video_standing[:points]}
+  end
+
+  def orda_count_formula(length, position)
+    length - position
+  end
+
+  def tally candidate_videos, preferences
+    preferences.inject({}) do |points_hash, preference|
+      preference_points = orda_count_formula(candidate_videos.length, preference.position)
+      id = preference.candidate_video.id
+      points_hash[id] = (points_hash[id] || 0 ) + preference_points
+      points_hash
     end
   end
 end
