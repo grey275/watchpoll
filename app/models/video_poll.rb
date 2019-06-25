4398ed036f7f6e04882953d3c460d88f88de1dc0
nvngsys <jack.brennan@nvngsystems.com>
2019-06-25 10:17:22 -0700

    Changes to preference order

--- app/models/video_poll.rb
+++ app/models/video_poll.rb
@@ -4,13 +4,19 @@ class VideoPoll < ApplicationRecord
   has_many :preference_orders
 
   def standings
-    all_user_preferences = self.preference_orders
+
+    user_sessions = self.preference_orders.user_sessions
+
+    user_preference_orders = user_sessions.map do |user_session|
+      user_session.preference_orders.last
+    end
+
     video_points_hash = self.candidate_videos.inject({}) do |video_points_hash, video|
       video_points_hash[video.id] = 0
       video_points_hash
     end
 
-    all_user_preferences
+    user_preference_orders
       .each do |order|
         user_preferences = order.preferences
         user_preferences
@@ -23,6 +29,7 @@ class VideoPoll < ApplicationRecord
             video_points_hash[preference.candidate_video_id] += points
           end
       end
+
     video_points_hash.keys.map do |key|
       points = video_points_hash[key]
       video_uid = CandidateVideo.find(key).video_uid
