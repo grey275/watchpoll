ffe1f2cfb2420f370041e3ecdcc634bb067ee8e1
grey275 <pjvanderpol@gmail.com>
2019-06-26 16:08:43 -0700

    add poll generation
    Co-authored-by: nvngsys <jack.brennan@nvngsystems.com>

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,9 +4,44 @@ class Room < ApplicationRecord
   has_one :candidate_video
   belongs_to :playlist
 
+  def get_unplayed_videos
+    played_videos  = self.video_polls
+      .select {|video_poll| video_poll.played_video_id }
+      .map {|video_poll| video_poll.played_video_id}
+      .map {|played_video_id| Video.find(played_video_id)}
+
+    self.playlist.videos.select do |video|
+      !played_videos.include? video
+    end
+  end
+
+  def generate_video_poll
+    # randomly pull out 6 videos
+    video_poll = VideoPoll.create(
+      room: self
+    )
+    unplayed_videos = get_unplayed_videos
+
+    6.times.each do
+      video_index = rand(unplayed_videos.length)
+      video = unplayed_videos[video_index]
+      unplayed_videos.pop(video_index)
+      CandidateVideo.create(
+        video: video,
+        video_poll: video_poll,
+      )
+    end
+    video_poll
+  end
+
   def current_video
     if self.video_polls.count > 1
-      return self.video_polls.second_to_last.played_video_id.video_uid
+      video_id = self.video_polls.second_to_last.played_video_id
+      Video.find video_id
     end
+  end
+
+  def current_user_sessions
+    self.user_sessions.where end: nil
   end
 end
