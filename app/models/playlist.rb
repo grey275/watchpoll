8f973f2dedfb1fe0b91dc14db5f707afe4b3e011
nvngsys <jack.brennan@nvngsystems.com>
2019-06-26 11:34:07 -0700

    Changes to seeding files

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,4 @@
 class Playlist < ApplicationRecord
-  has_many: videos
-  has_many: rooms
+  has_many :videos
+  has_many :rooms
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,7 @@ class Room < ApplicationRecord
   has_many :user_sessions
   has_many :video_polls
   has_one :candidate_video
-  belongs_to :playlist_id
+  belongs_to :playlist
 
   def current_video
     if self.video_polls.count > 1
