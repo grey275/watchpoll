62aa422006aa79d51c5d3b200a8dfa70df967a42
nvngsys <jack.brennan@nvngsystems.com>
2019-06-30 19:56:37 -0700

    Creation of room index page

--- client/src/RoomIndex.js
+++ client/src/RoomIndex.js
@@ -1,14 +1,50 @@
 import React from 'react'
+import Axios from 'axios'
 import { Link } from 'react-router-dom';
 
-const RoomIndex = () => (
 
+class RoomIndex extends React.Component {
+  constructor(props){
+      super(props)
+      this.state = { rooms: []}
+  }
+  getRooms(){
+    Axios.get('/api/rooms')
+    .then(response => {
+      this.setState({rooms: response.data})
+    })
+    .catch(error => console.log(error))
+  }
 
-    <div>
-        <Link to='/'>Placeholder - Back to Room</Link>
- 
-    </div>
+  componentDidMount(){
+    this.getRooms()
+  }
 
-)
+  render(){
+      return(
+        <div>
+            <div>
+                <Link to='/'>Return to Room</Link>
+            </div>
+
+            <div className="roomWrapper">
+                <ul className="roomList">
+                    {this.state.rooms.map((room) => {
+                        return(
+                            <React.Fragment>
+
+                            <p><Link to='/'>{room.room_name}</Link></p>
+                            <p>Playlist: {room.playlist_uid}</p> 
+
+                            </React.Fragment>
+                        );
+                    })}
+                </ul>
+
+            </div>
+        </div>
+      )
+  }
+}
 
 export default RoomIndex
