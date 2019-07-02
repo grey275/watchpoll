f45d1d6b38482e91bbc0d3eeaab4bbc6f90c2baf
nvngsys <jack.brennan@nvngsystems.com>
2019-07-02 10:36:20 -0700

    Changes to RoomIndex

--- client/src/RoomIndex.js
+++ client/src/RoomIndex.js
@@ -23,18 +23,22 @@ const RoomCard = ({room}) => {
 class RoomIndex extends React.Component {
   constructor(props){
     super(props)
-    this.state = { rooms: []}
+    this.state = { 
+      rooms: [],
+      playlist: []
+    }
   }
   getRooms() {
-    Axios.get('/api/rooms')
-    .then(response => {
-      this.setState({rooms: response.data})
-    })
-    .catch(error => console.log(error))
+    return Axios.get('/api/rooms')
+    // Axios.get('/api/rooms')
+    // .then(response => {
+    //   this.setState({rooms: response.data})
+    // })
+    // .catch(error => console.log(error))
   }
-
-  getPlaylistVideoDetail = async(current_video_ids) => {
-    //console.log('call to getPlaylistVideoDetail ');
+  
+  getRoomVideoDetail = async(current_video_ids) => {
+    //console.log('call to getRoomVideoDetail ');
     await this.props.gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest");
     //const current_video_ids = this.state.rooms.map(room => room.current_video_id);
     const response = await this.props.gapi.client.youtube.videos.list({
@@ -42,34 +46,44 @@ class RoomIndex extends React.Component {
       id: current_video_ids.join(),
     })
     
-
+    
     console.log('items: ', response.result.items)
-    return response.result.items;
+    return response.result.items.map(item => item.snippet);
   }
-
-
-
+  
+  
+  
   componentDidMount(){
     this.getRooms()
-    //const playlist_items = this.getPlaylistVideoDetail();
-    // const current_video_ids = this.state.rooms.map(room => room.current_video_id);
-    // console.log(current_video_ids.length);
-    // const playlist_items = this.getPlaylistVideoDetail(current_video_ids);    
+    .then(response => {
+      console.log(response)
+      const current_video_ids = response.data.map(room => room.current_video_id);
+      console.log("Testing curr video ",  current_video_ids.length);
+      this.getRoomVideoDetail(current_video_ids)  
+       .then(playlist_items => {
+         console.log(playlist_items)
+      //    this.setState({playlist:  response.data, })
+      })
+      this.setState({rooms: response.data, })
+
+    })
+    .catch(error => console.log(error))
   }
 
   render(){
     const { rooms } = this.state;
     const { gapi } = this.props;
 
-    const current_video_ids = rooms.map(room => room.current_video_id);
-    console.log(current_video_ids.length);
-    const playlist_items = this.getPlaylistVideoDetail(current_video_ids);
+    // const current_video_ids = rooms.map(room => room.current_video_id);
+    // console.log(current_video_ids.length);
+    // const playlist_items = this.getRoomVideoDetail(current_video_ids);
     
+
     console.log('gapi: ', gapi)
     const room_links = rooms.map((room) => {
       console.log('room: ', room);
 
-      // let hi = <getPlaylistVideoDetail video_id={room.current_video_id} />
+      // let hi = <getRoomVideoDetail video_id={room.current_video_id} />
       // console.log('hi = ', hi);
       
       return(
