c42999552fcea5d1be7ecd02e7759efcbfd5afc9
grey275 <pjvanderpol@gmail.com>
2019-07-03 10:47:19 -0700

    include nuke utility for user sessions

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -3,5 +3,11 @@ class UserSession < ApplicationRecord
   belongs_to :user
   has_many :preference_orders
   alias_attribute :start, :created_at
+
+  def self.nuke
+    Preference.destroy_all
+    PreferenceOrder.destroy_all
+    UserSession.destroy_all
+  end
 end
 #session.end.nil?
